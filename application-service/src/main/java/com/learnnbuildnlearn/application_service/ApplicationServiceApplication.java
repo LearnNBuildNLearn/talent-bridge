509525a0a3bf3b8c698f268019a15bba0abb3b0d
praveenukkoji <praveenukkoji@gmail.com>
2025-06-24 19:16:00 +0530

    application-service registering to eureka-server

--- application-service/src/main/java/com/learnnbuildnlearn/application_service/ApplicationServiceApplication.java
+++ application-service/src/main/java/com/learnnbuildnlearn/application_service/ApplicationServiceApplication.java
@@ -2,11 +2,13 @@ package com.learnnbuildnlearn.application_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApplicationServiceApplication {
 
